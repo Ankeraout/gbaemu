c473c20ec1f15a3e665414b87890764a0bcc87b3
Ankeraout <ankeraout@yahoo.com>
2020-01-05 22:20:52 +0100

    Fixed thumb multiple load/save opcodes behavior.

--- src/gbaemu/gba/cpu/impl/thumb/mls.cpp
+++ src/gbaemu/gba/cpu/impl/thumb/mls.cpp
@@ -4,20 +4,13 @@
 
 #include <cstdint>
 
-#define DEFINE_MLS_OPCODE(name, operation) \
+#define DEFINE_MLS_OPCODE(name, body) \
     void opcode_ ## name ## mia(uint16_t opcode) { \
         uint16_t Rb = (opcode & 0x0700) >> 8; \
         uint16_t Rlist = opcode & 0x00ff; \
         uint32_t Rb_v = registerRead(Rb) & 0xfffffffc; \
         \
-        for(unsigned int i = 0; i < 8; i++) { \
-            if(Rlist & 0x01) { \
-                operation; \
-                Rb_v += 4; \
-            } \
-            \
-            Rlist >>= 1; \
-        } \
+        body; \
         \
         registerWrite(Rb, Rb_v); \
     }
@@ -25,11 +18,25 @@
 namespace gbaemu::gba::cpu::impl::thumb::mls {
     DEFINE_MLS_OPCODE(
         st,
-        mmu::write32(Rb_v, registerRead(i))
+        for(int i = 7; i >= 0; i--) {
+            if(Rlist & 0x0080) {
+                mmu::write32(Rb_v, registerRead(i));
+                Rb_v += 4;
+            }
+            
+            Rlist <<= 1;
+        }
     )
 
     DEFINE_MLS_OPCODE(
         ld,
-        registerWrite(i, mmu::read32(Rb_v))
+        for(int i = 0; i < 8; i++) {
+            if(Rlist & 0x0001) {
+                registerWrite(i, mmu::read32(Rb_v));
+                Rb_v += 4;
+            }
+            
+            Rlist >>= 1;
+        }
     )
 }
