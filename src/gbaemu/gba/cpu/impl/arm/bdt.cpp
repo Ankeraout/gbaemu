ac2e7926729991a7107f652edd08744f5b8abc4f
Ankeraout <ankeraout@yahoo.com>
2020-01-23 21:49:37 +0100

    Started rewriting BDT opcodes ARMwrestler's LDM test doesn't pass anymore

--- src/gbaemu/gba/cpu/impl/arm/bdt.cpp
+++ src/gbaemu/gba/cpu/impl/arm/bdt.cpp
@@ -4,8 +4,8 @@
 #include <gbaemu/gba/cpu/impl/arm/bdt.hpp>
 #include <gbaemu/gba/mmu.hpp>
 
-#define DEFINE_BDT_OPCODE(name, header, body, footer) \
-    void opcode_ ## name(uint32_t opcode) { \
+#define DEFINE_BDT_OPCODE_SINGLE(opcodeName, header, rlistEmpty, body, footer) \
+    void opcode_ ## opcodeName(uint32_t opcode) { \
         uint32_t Rn = (opcode & 0x000f0000) >> 16; \
         uint32_t Rn_v = registerRead(Rn); \
         uint32_t Rlist = opcode & 0x0000ffff; \
@@ -13,22 +13,27 @@
         \
         header; \
         \
-        for(unsigned int i = 0; i < 16; i++) { \
-            if(Rlist & 0x0001) { \
-                body; \
-                accessAddress += 4; \
+        if(Rlist) { \
+            for(int i = 0; i < 16; i++) { \
+                if(Rlist & 0x0001) { \
+                    body; \
+                    accessAddress += 4; \
+                } \
+                \
+                Rlist >>= 1; \
             } \
-            \
-            Rlist >>= 1; \
+        } else { \
+            rlistEmpty; \
         } \
         \
         footer; \
     }
 
-#define HEADER_LDM \
-    if(Rlist == 0) { \
-        registerWrite(CPU_REG_PC, mmu::read32(accessAddress)); \
-    }
+#define DEFINE_BDT_OPCODE(opcode, suffix, header, rlistEmpty, body, footer) \
+    DEFINE_BDT_OPCODE_SINGLE(opcode ## da ## suffix, HEADER_DA; header, rlistEmpty, body, footer) \
+    DEFINE_BDT_OPCODE_SINGLE(opcode ## db ## suffix, HEADER_DB; header, rlistEmpty, body, footer) \
+    DEFINE_BDT_OPCODE_SINGLE(opcode ## ia ## suffix, HEADER_IA; header, rlistEmpty, body, footer) \
+    DEFINE_BDT_OPCODE_SINGLE(opcode ## ib ## suffix, HEADER_IB; header, rlistEmpty, body, footer)
 
 #define HEADER_DA \
     Rn_v -= Rcount << 2; \
@@ -45,6 +50,11 @@
 #define HEADER_IB \
     uint32_t accessAddress = Rn_v + 4; \
     Rn_v += Rcount << 2
+
+#define HEADER_LDM \
+    if(Rlist == 0) { \
+        registerWrite(CPU_REG_PC, mmu::read32(accessAddress)); \
+    }
 
 #define HEADER_LDM_S \
     int accessMode; \
@@ -70,7 +80,13 @@
 #define BODY_STM_S \
     mmu::write32(accessAddress, registerRead(i, PSR_MODE_USR) + OFFSET_R15)
 
-#define DO_WRITEBACK \
+#define RLISTEMPTY_STM \
+    mmu::write32(accessAddress, registerRead(CPU_REG_PC))
+
+#define RLISTEMPTY_LDM \
+    registerWrite(CPU_REG_PC, mmu::read32(accessAddress))
+
+#define FOOTER_WRITEBACK \
     registerWrite(Rn, Rn_v)
 
 #define FOOTER_LDM_S \
@@ -93,37 +109,13 @@ namespace gbaemu::gba::cpu::impl::arm::bdt {
         return hammingWeight;
     }
 
-    DEFINE_BDT_OPCODE(ldmda, HEADER_DA; HEADER_LDM, BODY_LDM, )
-    DEFINE_BDT_OPCODE(ldmdaw, HEADER_DA; HEADER_LDM; DO_WRITEBACK, BODY_LDM, )
-    DEFINE_BDT_OPCODE(ldmdas, HEADER_DA; HEADER_LDM; HEADER_LDM_S, BODY_LDM_S, FOOTER_LDM_S)
-    DEFINE_BDT_OPCODE(ldmdasw, HEADER_DA; HEADER_LDM; HEADER_LDM_S; DO_WRITEBACK, BODY_LDM_S, FOOTER_LDM_S)
-    DEFINE_BDT_OPCODE(ldmia, HEADER_IA; HEADER_LDM, BODY_LDM, )
-    DEFINE_BDT_OPCODE(ldmiaw, HEADER_IA; HEADER_LDM; DO_WRITEBACK, BODY_LDM, )
-    DEFINE_BDT_OPCODE(ldmias, HEADER_IA; HEADER_LDM; HEADER_LDM_S, BODY_LDM_S, FOOTER_LDM_S)
-    DEFINE_BDT_OPCODE(ldmiasw, HEADER_IA; HEADER_LDM; HEADER_LDM_S; DO_WRITEBACK, BODY_LDM_S, FOOTER_LDM_S)
-    DEFINE_BDT_OPCODE(ldmdb, HEADER_DB; HEADER_LDM, BODY_LDM, )
-    DEFINE_BDT_OPCODE(ldmdbw, HEADER_DB; HEADER_LDM; DO_WRITEBACK, BODY_LDM, )
-    DEFINE_BDT_OPCODE(ldmdbs, HEADER_DB; HEADER_LDM; HEADER_LDM_S, BODY_LDM_S, FOOTER_LDM_S)
-    DEFINE_BDT_OPCODE(ldmdbsw, HEADER_DB; HEADER_LDM; HEADER_LDM_S; DO_WRITEBACK, BODY_LDM_S, FOOTER_LDM_S)
-    DEFINE_BDT_OPCODE(ldmib, HEADER_IB; HEADER_LDM, BODY_LDM, )
-    DEFINE_BDT_OPCODE(ldmibw, HEADER_IB; HEADER_LDM; DO_WRITEBACK, BODY_LDM, )
-    DEFINE_BDT_OPCODE(ldmibs, HEADER_IB; HEADER_LDM; HEADER_LDM_S, BODY_LDM_S, FOOTER_LDM_S)
-    DEFINE_BDT_OPCODE(ldmibsw, HEADER_IB; HEADER_LDM; HEADER_LDM_S; DO_WRITEBACK, BODY_LDM_S, FOOTER_LDM_S)
+    DEFINE_BDT_OPCODE(stm, , , RLISTEMPTY_STM, BODY_STM, )
+    DEFINE_BDT_OPCODE(stm, s, , RLISTEMPTY_STM, BODY_STM_S, )
+    DEFINE_BDT_OPCODE(stm, w, , RLISTEMPTY_STM, BODY_STM, FOOTER_WRITEBACK)
+    DEFINE_BDT_OPCODE(stm, sw, , RLISTEMPTY_STM, BODY_STM_S, FOOTER_WRITEBACK)
 
-    DEFINE_BDT_OPCODE(stmda, HEADER_DA, BODY_STM, )
-    DEFINE_BDT_OPCODE(stmdaw, HEADER_DA, BODY_STM, DO_WRITEBACK)
-    DEFINE_BDT_OPCODE(stmdas, HEADER_DA, BODY_STM_S, )
-    DEFINE_BDT_OPCODE(stmdasw, HEADER_DA, BODY_STM_S, DO_WRITEBACK)
-    DEFINE_BDT_OPCODE(stmia, HEADER_IA, BODY_STM, )
-    DEFINE_BDT_OPCODE(stmiaw, HEADER_IA, BODY_STM, DO_WRITEBACK)
-    DEFINE_BDT_OPCODE(stmias, HEADER_IA, BODY_STM_S, )
-    DEFINE_BDT_OPCODE(stmiasw, HEADER_IA, BODY_STM_S, DO_WRITEBACK)
-    DEFINE_BDT_OPCODE(stmdb, HEADER_DB, BODY_STM, )
-    DEFINE_BDT_OPCODE(stmdbw, HEADER_DB, BODY_STM, DO_WRITEBACK)
-    DEFINE_BDT_OPCODE(stmdbs, HEADER_DB, BODY_STM_S, )
-    DEFINE_BDT_OPCODE(stmdbsw, HEADER_DB, BODY_STM_S, DO_WRITEBACK)
-    DEFINE_BDT_OPCODE(stmib, HEADER_IB, BODY_STM, )
-    DEFINE_BDT_OPCODE(stmibw, HEADER_IB, BODY_STM, DO_WRITEBACK)
-    DEFINE_BDT_OPCODE(stmibs, HEADER_IB, BODY_STM_S, )
-    DEFINE_BDT_OPCODE(stmibsw, HEADER_IB, BODY_STM_S, DO_WRITEBACK)
+    DEFINE_BDT_OPCODE(ldm, , HEADER_LDM, RLISTEMPTY_LDM, BODY_LDM, )
+    DEFINE_BDT_OPCODE(ldm, s, HEADER_LDM; HEADER_LDM_S, RLISTEMPTY_LDM, BODY_LDM_S, FOOTER_LDM_S)
+    DEFINE_BDT_OPCODE(ldm, w, HEADER_LDM, RLISTEMPTY_LDM, BODY_LDM, FOOTER_WRITEBACK)
+    DEFINE_BDT_OPCODE(ldm, sw, HEADER_LDM; HEADER_LDM_S, RLISTEMPTY_LDM, BODY_LDM_S, FOOTER_WRITEBACK; FOOTER_LDM_S)
 }
