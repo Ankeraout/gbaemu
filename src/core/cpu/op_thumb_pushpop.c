e52a410c4241c54f0f907d33c25c99415a5bfe24
Ankeraout <ankeraout@yahoo.com>
2023-09-05 23:31:55 +0200

    Implemented thumb PUSH/POP with empty rlist behavior

--- src/core/cpu/op_thumb_pushpop.c
+++ src/core/cpu/op_thumb_pushpop.c
@@ -8,36 +8,63 @@ void cpuOpcodeThumbPushPop(uint16_t p_opcode) {
     const bool l_isLoad = (p_opcode & (1 << 11)) != 0;
     const bool l_storeLrLoadPc = (p_opcode & (1 << 8)) != 0;
     const int l_registerCount = hammingWeight8(p_opcode) + (l_storeLrLoadPc ? 1 : 0);
+    const bool l_isRlistEmpty = (p_opcode & 0xff) == 0;
     const uint32_t l_base = g_cpuRegisterR[13];
 
-    uint32_t l_offset = l_base;
-
-    if(!l_isLoad) {
-        l_offset -= 4 * l_registerCount;
-    }
-
-    // loop
-    for(int l_registerIndex = 0; l_registerIndex < 8; l_registerIndex++) {
-        const uint32_t l_mask = 1 << l_registerIndex;
-
-        if((p_opcode & l_mask) != 0) {
-            if(l_isLoad) {
-                g_cpuRegisterR[l_registerIndex] = busRead32(l_offset);
-            } else {
-                busWrite32(l_offset, g_cpuRegisterR[l_registerIndex]);
-            }
-
-            l_offset += 4;
-        }
-    }
-
-    if(l_storeLrLoadPc) {
+    if(l_isRlistEmpty) {
         if(l_isLoad) {
-            cpuJump(busRead32(l_offset));
+            cpuJump(busRead32(l_base));
         } else {
-            busWrite32(l_offset, g_cpuRegisterR[14]);
+            busWrite32(l_base, g_cpuRegisterR[15] + 2);
         }
+
+        cpuWriteRegister(13, l_base + (l_isLoad ? 0x40 : -0x40));
+    } else {
+        uint32_t l_offset = l_base;
+
+        if(!l_isLoad) {
+            l_offset -= 4 * l_registerCount;
+        }
+
+        // loop
+        for(int l_registerIndex = 0; l_registerIndex < 8; l_registerIndex++) {
+            const uint32_t l_mask = 1 << l_registerIndex;
+
+            if((p_opcode & l_mask) != 0) {
+                if(l_isLoad) {
+                    g_cpuRegisterR[l_registerIndex] = busRead32(l_offset);
+                } else {
+                    busWrite32(l_offset, g_cpuRegisterR[l_registerIndex]);
+                }
+
+                l_offset += 4;
+            }
+        }
+
+        if(l_storeLrLoadPc) {
+            if(l_isLoad) {
+                cpuJump(busRead32(l_offset));
+            } else {
+                busWrite32(l_offset, g_cpuRegisterR[14]);
+            }
+        }
+
+        cpuWriteRegister(13, l_base + (l_isLoad ? 4 : -4) * l_registerCount);
+    }
+}
+
+static inline uint16_t getFirstRegisterInList(uint16_t p_opcode) {
+    uint16_t l_registerIndex = 0;
+    uint16_t l_registerMask = 1;
+
+    while(l_registerIndex < 8) {
+        if((p_opcode & l_registerMask) != 0) {
+            break;
+        }
+
+        l_registerIndex++;
+        l_registerMask <<= 1;
     }
 
-    cpuWriteRegister(13, l_base + (l_isLoad ? 4 : -4) * l_registerCount);
+    return l_registerIndex;
 }
