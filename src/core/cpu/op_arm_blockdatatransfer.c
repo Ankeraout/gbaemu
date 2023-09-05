7e1e82fd7ffb422bb550899c66bdd6cfcd5854b8
Ankeraout <ankeraout@yahoo.com>
2023-09-05 19:44:42 +0200

    Fixed STM writeback value when Rn is in Rlist

--- src/core/cpu/op_arm_blockdatatransfer.c
+++ src/core/cpu/op_arm_blockdatatransfer.c
@@ -5,13 +5,18 @@
 #include "core/cpu/bitops.h"
 #include "core/cpu/cpu.h"
 
+static inline uint32_t getFirstRegisterInList(uint32_t p_opcode);
+
 void cpuOpcodeArmLdmStm(uint32_t p_opcode) {
-    const bool l_isRlistEmpty = (p_opcode & 0xffff) == 0;
     const bool l_isPreIndexing = (p_opcode & (1 << 24)) != 0;
     const bool l_isUp = (p_opcode & (1 << 23)) != 0;
     const bool l_writeBack = (p_opcode & (1 << 21)) != 0;
     const bool l_isLoad = (p_opcode & (1 << 20)) != 0;
     const uint32_t l_rn = (p_opcode >> 16) & 0xf;
+    const bool l_isRlistEmpty = (p_opcode & 0xffff) == 0;
+    const uint32_t l_firstRegisterInList = getFirstRegisterInList(p_opcode);
+    const bool l_rnIsFirstRegisterInList = l_rn == l_firstRegisterInList;
+
     const uint32_t l_base = g_cpuRegisterR[l_rn];
 
     if(l_isRlistEmpty) {
@@ -62,16 +67,15 @@ void cpuOpcodeArmLdmStm(uint32_t p_opcode) {
             l_offset -= 4 * l_registerCount;
         }
 
-        // loop
-        for(int l_registerIndex = 0; l_registerIndex < 16; l_registerIndex++) {
-            const uint32_t l_mask = (1 << l_registerIndex);
+        uint32_t l_registerIndex = 0;
+        uint32_t l_registerMask = 1;
 
-            if((p_opcode & l_mask) != 0) {
+        while(l_registerIndex < 16) {
+            if((p_opcode & l_registerMask) != 0) {
                 if(l_isLoad) {
                     if(l_loadPsrOrForceUserMode) {
                         if(l_isPcInList) {
                             if(l_registerIndex == 15) {
-                                // SPSR => CPSR
                                 cpuSetCpsr(cpuGetSpsr());
                             }
                         } else {
@@ -83,25 +87,54 @@ void cpuOpcodeArmLdmStm(uint32_t p_opcode) {
                 } else {
                     if(l_loadPsrOrForceUserMode) {
                         if(l_registerIndex == 15) {
-                            busWrite32(l_offset, cpuReadRegisterUsr(15) + 4);
+                            busWrite32(l_offset, g_cpuRegisterR[15] + 4);
                         } else {
                             busWrite32(l_offset, cpuReadRegisterUsr(l_registerIndex));
                         }
                     } else {
+                        uint32_t l_value;
+
                         if(l_registerIndex == 15) {
-                            busWrite32(l_offset, g_cpuRegisterR[15] + 4);
+                            l_value = g_cpuRegisterR[15] + 4;
+                        } else if(
+                            (l_registerIndex == l_rn)
+                            && !l_rnIsFirstRegisterInList
+                            && l_writeBack
+                        ) {
+                            l_value = l_base + (l_isUp ? 4 : -4) * l_registerCount;
                         } else {
-                            busWrite32(l_offset, g_cpuRegisterR[l_registerIndex]);
+                            l_value = g_cpuRegisterR[l_registerIndex];
                         }
+
+                        busWrite32(l_offset, l_value);
                     }
                 }
 
                 l_offset += 4;
             }
+
+            l_registerIndex++;
+            l_registerMask <<= 1;
         }
 
-        if(l_writeBack) {
+        if(l_writeBack && !(l_isLoad && ((p_opcode & (1 << l_rn)) != 0))) {
             cpuWriteRegister(l_rn, l_base + (l_isUp ? 4 : -4) * l_registerCount);
         }
     }
+}
+
+static inline uint32_t getFirstRegisterInList(uint32_t p_opcode) {
+    uint32_t l_registerIndex = 0;
+    uint32_t l_registerMask = 1;
+
+    while(l_registerIndex < 16) {
+        if((p_opcode & l_registerMask) != 0) {
+            break;
+        }
+
+        l_registerIndex++;
+        l_registerMask <<= 1;
+    }
+
+    return l_registerIndex;
 }
