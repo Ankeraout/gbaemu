c74d48513054c4e9ca9ba8096fdbbfa59fa5868a
Ankeraout <ankeraout@yahoo.com>
2023-09-05 15:41:33 +0200

    Implemented ARM block data transfer opcodes with empty rlist

--- src/core/cpu/op_arm_blockdatatransfer.c
+++ src/core/cpu/op_arm_blockdatatransfer.c
@@ -5,83 +5,103 @@
 #include "core/cpu/bitops.h"
 #include "core/cpu/cpu.h"
 
-static inline void cpuOpcodeArmLdmStmEmptyRlist(uint32_t p_opcode);
-
 void cpuOpcodeArmLdmStm(uint32_t p_opcode) {
     const bool l_isRlistEmpty = (p_opcode & 0xffff) == 0;
-
-    if(l_isRlistEmpty) {
-        cpuOpcodeArmLdmStmEmptyRlist(p_opcode);
-        return;
-    }
-
     const bool l_isPreIndexing = (p_opcode & (1 << 24)) != 0;
     const bool l_isUp = (p_opcode & (1 << 23)) != 0;
-    const bool l_loadPsrOrForceUserMode = (p_opcode & (1 << 22)) != 0;
     const bool l_writeBack = (p_opcode & (1 << 21)) != 0;
     const bool l_isLoad = (p_opcode & (1 << 20)) != 0;
-    const bool l_isPcInList = (p_opcode & (1 << 15)) != 0;
     const uint32_t l_rn = (p_opcode >> 16) & 0xf;
-
-    const int l_registerCount = hammingWeight16(p_opcode);
     const uint32_t l_base = g_cpuRegisterR[l_rn];
 
-    uint32_t l_offset = l_base;
+    if(l_isRlistEmpty) {
+        uint32_t l_offset;
 
-    if(l_isUp == l_isPreIndexing) {
-        l_offset += 4;
-    }
-
-    if(!l_isUp) {
-        l_offset -= 4 * l_registerCount;
-    }
-
-    // loop
-    for(int l_registerIndex = 0; l_registerIndex < 16; l_registerIndex++) {
-        const uint32_t l_mask = (1 << l_registerIndex);
-
-        if((p_opcode & l_mask) != 0) {
-            if(l_isLoad) {
-                if(l_loadPsrOrForceUserMode) {
-                    if(l_isPcInList) {
-                        if(l_registerIndex == 15) {
-                            // SPSR => CPSR
-                            cpuSetCpsr(cpuGetSpsr());
-                        }
-                    } else {
-                        cpuWriteRegisterUsr(l_registerIndex, busRead32(l_offset));
-                    }
-                } else {
-                    cpuWriteRegister(l_registerIndex, busRead32(l_offset));
-                }
+        if(l_isPreIndexing) {
+            if(l_isUp) {
+                l_offset = 4;
             } else {
-                if(l_loadPsrOrForceUserMode) {
-                    if(l_registerIndex == 15) {
-                        busWrite32(l_offset, cpuReadRegisterUsr(15) + 4);
-                    } else {
-                        busWrite32(l_offset, cpuReadRegisterUsr(l_registerIndex));
-                    }
-                } else {
-                    if(l_registerIndex == 15) {
-                        busWrite32(l_offset, g_cpuRegisterR[15] + 4);
-                    } else {
-                        busWrite32(l_offset, g_cpuRegisterR[l_registerIndex]);
-                    }
-                }
+                l_offset = -0x40;
             }
+        } else {
+            if(l_isUp) {
+                l_offset = 0;
+            } else {
+                l_offset = -0x3c;
+            }
+        }
 
+        const uint32_t l_address = l_base + l_offset;
+
+        if(l_isLoad) {
+            cpuJump(busRead32(l_address));
+        } else {
+            busWrite32(l_address, g_cpuRegisterR[15] + 4);
+        }
+
+        if(l_writeBack) {
+            if(l_isUp) {
+                cpuWriteRegister(l_rn, l_address + 0x40);
+            } else {
+                cpuWriteRegister(l_rn, l_address - 0x40);
+            }
+        }
+    } else {
+        const bool l_loadPsrOrForceUserMode = (p_opcode & (1 << 22)) != 0;
+        const bool l_isPcInList = (p_opcode & (1 << 15)) != 0;
+
+        const int l_registerCount = hammingWeight16(p_opcode);
+
+        uint32_t l_offset = l_base;
+
+        if(l_isUp == l_isPreIndexing) {
             l_offset += 4;
         }
+
+        if(!l_isUp) {
+            l_offset -= 4 * l_registerCount;
+        }
+
+        // loop
+        for(int l_registerIndex = 0; l_registerIndex < 16; l_registerIndex++) {
+            const uint32_t l_mask = (1 << l_registerIndex);
+
+            if((p_opcode & l_mask) != 0) {
+                if(l_isLoad) {
+                    if(l_loadPsrOrForceUserMode) {
+                        if(l_isPcInList) {
+                            if(l_registerIndex == 15) {
+                                // SPSR => CPSR
+                                cpuSetCpsr(cpuGetSpsr());
+                            }
+                        } else {
+                            cpuWriteRegisterUsr(l_registerIndex, busRead32(l_offset));
+                        }
+                    } else {
+                        cpuWriteRegister(l_registerIndex, busRead32(l_offset));
+                    }
+                } else {
+                    if(l_loadPsrOrForceUserMode) {
+                        if(l_registerIndex == 15) {
+                            busWrite32(l_offset, cpuReadRegisterUsr(15) + 4);
+                        } else {
+                            busWrite32(l_offset, cpuReadRegisterUsr(l_registerIndex));
+                        }
+                    } else {
+                        if(l_registerIndex == 15) {
+                            busWrite32(l_offset, g_cpuRegisterR[15] + 4);
+                        } else {
+                            busWrite32(l_offset, g_cpuRegisterR[l_registerIndex]);
+                        }
+                    }
+                }
+
+                l_offset += 4;
+            }
+        }
+
+        if(l_writeBack) {
+            cpuWriteRegister(l_rn, l_base + (l_isUp ? 4 : -4) * l_registerCount);
+        }
     }
-
-    if(l_writeBack) {
-        cpuWriteRegister(l_rn, l_base + (l_isUp ? 4 : -4) * l_registerCount);
-    }
-}
-
-static inline void cpuOpcodeArmLdmStmEmptyRlist(uint32_t p_opcode) {
-    const bool l_isLoad = (p_opcode & (1 << 20)) != 0;
-    const bool l_isPreIndexing = (p_opcode & (1 << 24)) != 0;
-    const bool l_isUp = (p_opcode & (1 << 23)) != 0;
-
 }
